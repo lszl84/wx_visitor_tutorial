719f5f555f4b7d0a0425cf4161fa4077ded12c3b
Luke <luke@justdevtutorials.com>
2023-05-25 13:50:45 +0200

    using structs for consistency

--- src/shapes/shapevisitor.h
+++ src/shapes/shapevisitor.h
@@ -1,12 +1,11 @@
 #pragma once
 
-class Circle;
-class Rect;
-class Path;
+struct Circle;
+struct Rect;
+struct Path;
 
-class ShapeVisitor
+struct ShapeVisitor
 {
-public:
     virtual void Visit(const Circle &circle) = 0;
     virtual void Visit(const Rect &rectangle) = 0;
     virtual void Visit(const Path &rectangle) = 0;
